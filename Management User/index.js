ccf882573c3cf4a8bf49011178c1d8298bf53178
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-15 16:44:35 +0600

    Update index.js

--- Management User/index.js
+++ Management User/index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const users=[
+  {id:1,name:'sakib',email:'sakib@gmail.com'},
+  {id:2,name:'Tamim',email:'tamim@gmail.com'},
+  {id:3,name:'Rakib',email:'rakib@gmail.com'},
+  {id:4,name:'Musfiq',email:'musi@gmail.com'}
+]
+
 app.get('/', (req, res) => {
   res.send('User management server is running')
+})
+
+app.get('/users',(req,res)=>{
+  res.send(users)
 })
 
 app.listen(port, () => {
