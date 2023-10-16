bc39e3bd41c3594e5934a20ac270bca04501d177
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-16 16:18:17 +0600

    Data delete Done

--- CRUD Server/index.js
+++ CRUD Server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors= require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware.
 app.use(cors());
@@ -34,14 +34,22 @@ async function run() {
       const cursor = userCollection.find()
       const result=await cursor.toArray();
       res.send(result);
-    })
+      })
 
       app.post('/users',async(req, res) => {
           const user=req.body;
           console.log('new user', user);
           const result = await userCollection.insertOne(user);
           res.send(result);
-})
+      })
+
+        app.delete('/users/:id', async (req, res) => {
+          const id=req.params.id;
+          console.log('delete user', id);
+          const query = { _id: new ObjectId(id)};
+          const result = await userCollection.deleteOne(query);
+          res.send(result);
+        });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
--- CRUD-Client/src/components/Users.jsx
+++ CRUD-Client/src/components/Users.jsx
@@ -3,12 +3,27 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
 
     const   Users = useLoaderData();
+
+    const handleDelete= _id =>{
+        console.log("delete", _id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: "DELETE",
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if (data.deletedCount>0) {
+                alert('deleted successfully')
+            }
+        })
+
+    }
     
     return (
         <div className="">
            <h2 className="text-5xl flex text-center justify-center"> Users:{Users.length}</h2>
             {
-                Users.map(user=> <p key={user._id}>{user.name} : {user.email}</p>)
+                Users.map(user=> <p key={user._id}>{user.name} : {user.email}  {user._id}<button onClick={()=>handleDelete(user._id)}>X</button></p>)
             }
         </div>
     );
