c4b9706db7f9bfda3e742e0f40d64016e1cb1fb6
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-16 09:19:30 +0600

    Update index.js

--- CRUD Server/index.js
+++ CRUD Server/index.js
@@ -1,12 +1,17 @@
 
 const express = require('express')
+const cors= require('cors');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+
+// middleware.
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Simple CRUD is available')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`simple crud is available on port, ${port}`)
 })
