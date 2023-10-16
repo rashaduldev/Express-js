5f56704b7ad47b39ffff1c7c99842d2d4e6d754a
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-16 15:22:28 +0600

    Data Read/get Done

--- CRUD-Client/src/components/Users.jsx
+++ CRUD-Client/src/components/Users.jsx
@@ -1,10 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+
 const Users = () => {
 
-
+    const   Users = useLoaderData();
     
     return (
-        <div>
-            Users:
+        <div className="">
+           <h2 className="text-5xl flex text-center justify-center"> Users:{Users.length}</h2>
+            {
+                Users.map(user=> <p key={user._id}>{user.name} : {user.email}</p>)
+            }
         </div>
     );
 };
